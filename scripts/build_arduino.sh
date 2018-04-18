e6040e5eea11e23a3c5c2aebd150a27bed83224b
Aiden Woodruff <aiden.woodruff@gmail.com>
2018-04-17 20:40:37 -0500

    Corrected PlatformIO parameters

--- scripts/build_arduino.sh
+++ scripts/build_arduino.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
+for r in arduino/refs/*; do
+  refs="${refs} --lib=${r}"
+done
+
+for s in arduino/source/*; do
+  sources="${sources} --lib=${s}"
+done
+
 for f in arduino/main/*; do
   echo "Building ${f}"
-  platformio ci --lib=arduino/refs/* --lib=arduino/source/* --board=uno ${f}
+  platformio ci ${refs} ${sources} --board=uno ${f}
 done
 
 for q in main/Final_Plain_Rain/*; do
-  platformio ci --lib=arduino/refs/* --board=uno ${q}
+  platformio ci ${refs} --board=uno ${q}
 done
